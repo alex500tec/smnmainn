fe483af29279a51a7dd4c8784f6c6eb7847f87a7
Ismael Gómez Rodríguez <ismael.gomez@tecvalles.mx>
2025-03-18 10:48:57 -0600

    Agregué responsividad, icono de informacion y comenzamos con WidgetDia

--- lib/custom_widgets/dia_principal.dart
+++ lib/custom_widgets/dia_principal.dart
@@ -6,6 +6,34 @@ class DiaPrincipal extends StatelessWidget {
   final ModeloPronostico dia;
   DiaPrincipal({super.key, required this.dia});
 
+  Widget Separador() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+          border: Border(
+        right: BorderSide(
+          color: Colors.black54,
+          width: 2,
+        ),
+      )),
+      child: SizedBox(
+        width: 40,
+        height: 40,
+      ),
+    );
+  }
+
+  Widget Elemento(String texto, IconData icono, String medida) {
+    return Column(
+      children: [
+        Text(texto),
+        Row(
+          children: [Icon(icono), Text(medida)],
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -21,20 +49,50 @@ class DiaPrincipal extends StatelessWidget {
             Text(
               dia.tmax,
               style: TextStyle(
-                fontSize: 80.0,
+                fontSize: 70.0,
                 color: Colors.redAccent,
               ),
             ),
             Text(
-              "/${dia.tmin} C",
+              "/${dia.tmin} ºC",
               style: TextStyle(
-                fontSize: 40.0,
+                fontSize: 35.0,
                 color: Colors.blueAccent,
               ),
             ),
           ]),
         ),
-        TextoGrisNegrita(texto: "Precipitación", medida: dia.prec)
+        MediaQuery.of(context).size.width <= 600
+            ? Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  children: [
+                    TextoGrisNegrita(
+                        texto: "Probabilidad de lluvia:", medida: dia.probprec),
+                    TextoGrisNegrita(
+                        texto: "Velocidad del viento:", medida: dia.velvien),
+                    TextoGrisNegrita(
+                        texto: "Dirección del viento:", medida: dia.dirvienc),
+                    TextoGrisNegrita(
+                        texto: "Ráfaga de viento:", medida: dia.raf),
+                  ],
+                ),
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Elemento("Lluvia", Icons.water_drop, "${dia.prec}lts/m2"),
+                  Separador(),
+                  Elemento("Probabilidad de lluvia", Icons.cloudy_snowing,
+                      "${dia.probprec}lts/m2"),
+                  Separador(),
+                  Elemento("Dirección del viento", Icons.arrow_outward,
+                      "${dia.dirvienc}kms/h"),
+                  Separador(),
+                  Elemento("Velocidad del viento", Icons.speed_outlined,
+                      "${dia.velvien}kms/h"),
+                ],
+              )
       ],
     );
   }
---
+++ lib/custom_widgets/widget_dia.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:smn/models/modelo_pronostico.dart';
+
+class WidgetDia extends StatelessWidget {
+  final ModeloPronostico pronostico;
+  final int index;
+
+  WidgetDia({
+    super.key,
+    required this.pronostico,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
